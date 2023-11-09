1d92245c74b389fb00e59032eb7a0dc0cc94c4e9
nyanyacyan <nyanyacyan@gmail.com>
2023-11-09 11:07:48 +0900

    不必要なものを削除

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import './App.css'; // Appのスタイルシートをインポート
 import Header from './components/Header'; // Headerのスタイルシートをインポート
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>nyanyacyan's Portfolio</title>
-        <link rel="icon" type="image/x-icon"　href="%PUBLIC_URL%/myfavicon.ico" />
-      </Helmet>
       <Header /> {/* Headerコンポーネントをレンダリング */}
       {/* 他のコンテンツ */}
     </div>
