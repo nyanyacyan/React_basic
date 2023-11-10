fdfd21f6161b9146c4a31678d261aec9f2f09b93
nyanyacyan <nyanyacyan@gmail.com>
2023-11-10 12:19:46 +0900

    TOPの大まかなデザイン完了 あとはアイコンを加えて動きをつける。

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css'; // Appのスタイルシートをインポート
 import Header from './components/Header'; // Headerのスタイルシートをインポート
+import Top from './components/Top';
 
 function App() {
   return (
     <div className="App">
       <Header /> {/* Headerコンポーネントをレンダリング */}
       {/* 他のコンテンツ */}
+      <Top />
     </div>
   );
 }
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,1 +1,33 @@
 // Top.js
+import React, { useState, useEffect } from "react";
+import './Top.css';
+
+const Top = () => {
+    const [scrollY, setScrollY] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollY(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true});
+
+        return() => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    },[]);
+
+    return(
+        <div className="topcontainer">
+            <div className="sky">
+                <img src="バック.jpg" alt="背景"/>
+                <h1>
+                    にゃにゃちゃん's　Lab
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Top;
